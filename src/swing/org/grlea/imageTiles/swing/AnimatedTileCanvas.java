e5e261bae95d0ffff8cf8bf4c5e36bb0299cfa9f
Graham Lea <graham@grahamlea.com>
2004-08-27 01:19:36 +0000

    Made AnimatedTileCanvas work.

--- src/swing/org/grlea/imageTiles/swing/AnimatedTileCanvas.java
+++ src/swing/org/grlea/imageTiles/swing/AnimatedTileCanvas.java
@@ -1,6 +1,6 @@
 package org.grlea.imageTiles.swing;
 
-// $Id: AnimatedTileCanvas.java,v 1.1 2004-08-23 22:47:56 grlea Exp $
+// $Id: AnimatedTileCanvas.java,v 1.2 2004-08-27 01:19:36 grlea Exp $
 // Copyright (c) 2004 Graham Lea. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,32 +16,147 @@ package org.grlea.imageTiles.swing;
 // limitations under the License.
 
 import org.grlea.imageTiles.AnimationController;
+import org.grlea.imageTiles.AnimationKitListener;
+import org.grlea.imageTiles.AnimationRenderKit;
+import org.grlea.imageTiles.Animator;
+import org.grlea.imageTiles.BackgroundPainter;
+import org.grlea.imageTiles.pipeline.ImageSource;
+import org.grlea.imageTiles.Painter;
+import org.grlea.imageTiles.pipeline.Pipeline;
+import org.grlea.imageTiles.pipeline.PipelineImageChanger;
+import org.grlea.imageTiles.pipeline.PipelineListener;
+import org.grlea.imageTiles.pipeline.PipelineTicker;
+import org.grlea.imageTiles.Placer;
+import org.grlea.imageTiles.SchedulerPausingAnimationKitListener;
+import org.grlea.imageTiles.TileRenderer;
 import org.grlea.imageTiles.TileSpace;
+import org.grlea.imageTiles.RenderKit;
+import org.grlea.imageTiles.AnimationKit;
+import org.grlea.imageTiles.animate.SlideAnimator;
+import org.grlea.imageTiles.background.ColourBackgroundPainter;
+import org.grlea.imageTiles.choose.RandomChooser;
+import org.grlea.imageTiles.paint.StaticPainter;
+import org.grlea.imageTiles.place.CentrePlacer;
+import org.grlea.util.FixedRateScheduler;
 
-import java.awt.Graphics2D;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Insets;
+import java.awt.Component;
+import java.awt.event.HierarchyEvent;
+import java.awt.event.HierarchyListener;
+import java.awt.image.VolatileImage;
+
+import javax.swing.JComponent;
 
 /**
  * <p></p>
  *
  * @author grlea
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
-public class 
+public class
 AnimatedTileCanvas
-extends TileCanvas
+extends Component
 {
-   private final AnimationController animationController;
+   private static final int DEFAULT_FRAME_RATE = 50;
+
+   private final TileSpace tileSpace;
+
+   private final FixedRateScheduler scheduler;
+
+   private Pipeline pipeline;
+
+   private VolatileImage buffer = null;
 
    public
-   AnimatedTileCanvas(TileSpace tileSpace, AnimationController animationController)
+   AnimatedTileCanvas(TileSpace tileSpace, ImageSource imageSource, TileRenderer renderer)
    {
-      super(tileSpace);
-      this.animationController = animationController;
+      this(tileSpace, imageSource, renderer, new SlideAnimator(tileSpace));
    }
 
-   protected void
-   paintTiles(Graphics2D graphics)
+   public
+   AnimatedTileCanvas(TileSpace tileSpace, ImageSource imageSource, TileRenderer renderer,
+                           Animator animator)
    {
-      animationController.paint(graphics);
+      this(tileSpace, imageSource, renderer, animator, DEFAULT_FRAME_RATE);
+   }
+
+   public
+   AnimatedTileCanvas(TileSpace tileSpace, ImageSource imageSource, TileRenderer renderer,
+                           Animator animator, int frameRate)
+   {
+      this.tileSpace = tileSpace;
+      BackgroundPainter backgroundPainter = new ColourBackgroundPainter(tileSpace);
+      Painter painter = new StaticPainter(tileSpace);
+      Placer placer = new CentrePlacer();
+      RandomChooser chooser = new RandomChooser(tileSpace);
+
+      AnimationKit animationController = new AnimationController(chooser, animator, painter);
+      RenderKit renderKit = new AnimationRenderKit(backgroundPainter, animator, painter);
+      RepaintComponentPipelineListener pipelineListener = new RepaintComponentPipelineListener(this);
+      Pipeline pipeline = new Pipeline(tileSpace, imageSource, placer, renderer, animationController,
+                                       renderKit, new PipelineListener[] {pipelineListener});
+      this.pipeline = pipeline;
+
+      PipelineTicker pipelineTicker = new PipelineTicker(pipeline);
+      scheduler = new FixedRateScheduler(pipelineTicker, frameRate);
+
+      AnimationKitListener schedulerPauser = new SchedulerPausingAnimationKitListener(scheduler);
+      animationController.addListener(schedulerPauser);
+
+      AnimationKitListener imageAdvancer = new PipelineImageChanger(pipeline);
+      animationController.addListener(imageAdvancer);
+   }
+
+   public void
+   start()
+   {
+      scheduler.start();
+   }
+
+   public void
+   stop()
+   {
+      scheduler.stop();
+   }
+
+   public Dimension
+   getPreferredSize()
+   {
+      return tileSpace.getSize();
+   }
+
+   public void
+   paint(Graphics graphics)
+   {
+      super.paint(graphics);
+
+      if (buffer == null)
+         createBuffer();
+
+      do
+      {
+         // VolatileImage handling code courtesy of the legendary Chet Haase
+         int validateCode = buffer.validate(getGraphicsConfiguration());
+         if (validateCode == VolatileImage.IMAGE_INCOMPATIBLE)
+            createBuffer();
+
+         pipeline.render(buffer.createGraphics());
+
+         graphics.drawImage(buffer, 0, 0, null);
+      }
+      while (buffer.contentsLost());
+   }
+
+   private void
+   createBuffer()
+   {
+      if (buffer != null)
+      {
+         buffer.flush();
+         buffer = null;
+      }
+      buffer = createVolatileImage(tileSpace.getWidth(), tileSpace.getHeight());
    }
 }
