ce6b14747ac53fe9377eb5f5eb8741065393adec
Graham Lea <graham@grahamlea.com>
2004-08-29 22:25:02 +0000

    Changed to use an internal VolatileImage.

--- src/core/org/grlea/imageTiles/paint/StaticPainter.java
+++ src/core/org/grlea/imageTiles/paint/StaticPainter.java
@@ -1,6 +1,6 @@
 package org.grlea.imageTiles.paint;
 
-// $Id: StaticPainter.java,v 1.2 2004-08-27 01:13:17 grlea Exp $
+// $Id: StaticPainter.java,v 1.3 2004-08-29 22:25:02 grlea Exp $
 // Copyright (c) 2004 Graham Lea. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,17 +19,16 @@ import org.grlea.imageTiles.RenderedTile;
 import org.grlea.imageTiles.Tile;
 import org.grlea.imageTiles.TileSpace;
 
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Color;
-import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
+import java.awt.image.VolatileImage;
 
 /**
  * <p></p>
  *
  * @author grlea
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class 
 StaticPainter
@@ -39,18 +38,15 @@ extends AbstractPainter
 
    private final RenderedTile[][] renderedTiles;
 
-   private final BufferedImage image;
+   private VolatileImage bufferImage = null;
 
-   private final Graphics2D graphics;
+   private Graphics2D bufferGraphics = null;
 
    public
    StaticPainter(TileSpace tileSpace)
    {
       this.tileSpace = tileSpace;
       this.renderedTiles = new RenderedTile[tileSpace.getRows()][tileSpace.getColumns()];
-      Dimension size = tileSpace.getSize();
-      this.image = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_ARGB);
-      this.graphics = image.createGraphics();
    }
 
    public void
@@ -58,7 +54,8 @@ extends AbstractPainter
    {
       Tile tile = renderedTile.getTile();
       renderedTiles[tile.getRow()][tile.getColumn()] = renderedTile;
-      renderedTile.paint(graphics);
+      if (bufferGraphics != null)
+         renderedTile.paint(bufferGraphics);
    }
 
    public void
@@ -72,8 +69,11 @@ extends AbstractPainter
             renderedTileRow[column] = null;
          }
       }
-      graphics.setBackground(new Color(0, 0, 0, 0));
-      graphics.clearRect(0, 0, tileSpace.getWidth(), tileSpace.getHeight());
+      if (bufferGraphics != null)
+      {
+         bufferGraphics.setBackground(new Color(0, 0, 0, 0));
+         bufferGraphics.clearRect(0, 0, tileSpace.getWidth(), tileSpace.getHeight());
+      }
    }
 
    public void
@@ -83,7 +83,58 @@ extends AbstractPainter
    public void
    render(Graphics2D graphics)
    {
-      graphics.drawImage(image, 0, 0, null);
+      if (bufferImage == null)
+         createBuffer(graphics);
+
+      do
+      {
+         int validateCode = bufferImage.validate(graphics.getDeviceConfiguration());
+         switch (validateCode)
+         {
+            case VolatileImage.IMAGE_INCOMPATIBLE:
+               createBuffer(graphics);
+               // Falls through
+            case VolatileImage.IMAGE_RESTORED:
+               internalRender();
+               // Falls through
+            case VolatileImage.IMAGE_OK:
+               graphics.drawImage(bufferImage, 0, 0, null);
+         }
+      }
+      while (bufferImage.contentsLost());
+   }
+
+   private void
+   createBuffer(Graphics2D graphics)
+   {
+      if (bufferImage != null)
+      {
+         bufferGraphics.dispose();
+         bufferGraphics = null;
+         bufferImage.flush();
+         bufferImage = null;
+      }
+      GraphicsConfiguration device = graphics.getDeviceConfiguration();
+      bufferImage = device.createCompatibleVolatileImage(tileSpace.getWidth(), tileSpace.getHeight());
+      bufferGraphics = bufferImage.createGraphics();
+   }
+
+   private void
+   internalRender()
+   {
+      bufferGraphics.setBackground(new Color(0, 0, 0, 0));
+      bufferGraphics.clearRect(0, 0, tileSpace.getWidth(), tileSpace.getHeight());
+
+      for (int row = 0; row < renderedTiles.length; row++)
+      {
+         RenderedTile[] renderedTilesRow = renderedTiles[row];
+         for (int column = 0; column < renderedTilesRow.length; column++)
+         {
+            RenderedTile renderedTile = renderedTilesRow[column];
+            if (renderedTile != null)
+               renderedTile.paint(bufferGraphics);
+         }
+      }
    }
 
    public TileSpace
